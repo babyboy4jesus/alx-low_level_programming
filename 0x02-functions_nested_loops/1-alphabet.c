d4e35f582b2ad3df2eba1d7299f710b2cc1b7267
Nwankwo Everest Chukwuebuka <nwankwoeverestchukwuebuka@gmail.com>
2022-12-30 17:21:23 -0800

    C is hard

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,21 +4,19 @@
  * print_alphabet - entry point
  *
  * Return: 0
- * */
- 
+ */
+
 void print_alphabet(void)
 
 {
 	char c = 'a';
 
 	while (c <= 'z')
-
 	{
 		_putchar(c);
-
 		c++;
-
 	}
 
 	_putchar('\n');
+
 }
