8418bd8ffc7862e51853f80dd3fb0744524e53f7
Nwankwo Everest Chukwuebuka <nwankwoeverestchukwuebuka@gmail.com>
2023-01-03 20:55:08 +0100

    C harder

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	while (s[i] != '\0') /*iterate through string*/
 	{
 	
-		for (j = 0; accept[j} != '\0'; j++) /*iterate through target*/
+		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 		{
 			if (s[i] == accept[j]) /*record & break at first match*/
 			{
@@ -25,7 +25,6 @@ unsigned int _strspn(char *s, char *accept)
 			if (accept[j + 1] == '\0' && s[i] != accept[j])
 			
 			return (matches);/*return if idx doesn't match*/
-
 		}
 
 		i++;
